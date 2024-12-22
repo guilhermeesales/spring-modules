604449dc2d6d418c48a1d433ad32a4b66b54c2f1
Guilherme Sales <guilhermessales11@gmail.com>
2024-12-22 15:53:13 -0300

    first commit

---
+++ learnspringsecurity/src/main/java/com/br/guilhermesales/learnspringsecurity/GreetingController.java
@@ -0,0 +1,15 @@
+package com.br.guilhermesales.learnspringsecurity;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+}
